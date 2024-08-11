bb6d3596ec1d82dd4e407105bdab86729497def3
ombima56 <ombimahillary6@gmail.com>
2024-08-11 03:32:14 +0300

    Add routes folder with routes.go for registering handlers

---
+++ routes/routes.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"net/http"
+
+	"groupie-trcker/handlers"
+)
+
+func RegisterRoutes() {
+	http.HandleFunc("/", handlers.IndexHandler)
+	http.HandleFunc("/artists", handlers.GetArtistsHandler)
+	http.HandleFunc("/locations", handlers.GetLocationsHandler)
+	http.HandleFunc("/dates", handlers.GetDatesHandler)
+	http.HandleFunc("/relations", handlers.GetRelationsHandler)
+	http.HandleFunc("/artists/", handlers.GetArtistByIDHandler)
+	http.HandleFunc("/artist/", handlers.ServeArtistDetails)
+}
