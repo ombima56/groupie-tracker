7ee24997b87b1fe1d4e9d31a8a9452f06f71b037
ombima56 <ombimahillary6@gmail.com>
2024-08-13 08:00:58 +0300

    feat: add http error handle for handle unknown paths

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"groupie-trcker/handlers"
+	"groupie-trcker/routes"
+
 	"log"
 	"net/http"
-
-	"groupie-trcker/routes"
+	"strings"
 )
 
 func main() {
 	routes.RegisterRoutes()
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
+	// Catch-all for undefined routes
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Check if the request is for the root path or favicon.ico
+		if r.URL.Path == "/" || r.URL.Path == "/favicon.ico" {
+			handlers.ServeArtists(w, r)
+			return
+		}
+		// Check if the request is for an artist details page
+		if strings.HasPrefix(r.URL.Path, "/artist/") {
+			handlers.ServeArtistDetails(w, r)
+			return
+		}
+		// If it's not a known route, use ErrorHandler for 404
+		handlers.ErrorHandler(w, "404 Page Not Found", http.StatusNotFound)
+	})
 
 	log.Println("Server starting on port http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
