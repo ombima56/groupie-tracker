5c81ba445af85692b1f6584bb88909532ac9392f
ombima56 <ombimahillary6@gmail.com>
2024-08-13 08:02:18 +0300

    chores: remove redudant handle

--- routes/routes.go
+++ routes/routes.go
@@ -7,10 +7,9 @@ import (
 )
 
 func RegisterRoutes() {
-	http.HandleFunc("/", handlers.IndexHandler)
 	http.HandleFunc("/artists", handlers.GetArtistsHandler)
+	http.HandleFunc("/artist/", handlers.ServeArtistDetails)
 	http.HandleFunc("/locations", handlers.GetLocationsHandler)
 	http.HandleFunc("/dates", handlers.GetDatesHandler)
 	http.HandleFunc("/relations", handlers.GetRelationsHandler)
-	http.HandleFunc("/artist/", handlers.ServeArtistDetails)
 }
